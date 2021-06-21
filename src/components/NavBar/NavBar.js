98971598495882f39b9995dde8b69ebda50ac09f
inshal ayaz <inshal.ayaz14@gmail.com>
2021-06-21 23:46:52 +0500

    Nav Bar Completed

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import "./Style.css"
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import useStyles from './Style'
+import Logo from '../../assets/My Picture.JPG'
+
+export default function NavBar() {
+    const classes = useStyles();
+  
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" className={classes.appbar}>
+            <Toolbar>
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                    <img src={Logo}  style={{height: "70px", borderRadius:"50%" }} alt="Inshal"/>
+                </IconButton>
+                <Typography variant="h6" className={classes.title}>
+                    Hire Me As A Developer Or A H@cker
+                </Typography>
+                <a href="#home"><Button color="inherit" >Home</Button></a>
+                <a href="#about"><Button color="inherit">About</Button></a>
+                <a href="#services"><Button color="inherit">Services</Button></a>
+                <a href="work"><Button color="inherit">Work</Button></a>
+                <a href="contact"><Button color="inherit">Contact</Button></a>
+            </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
---
+++ src/components/NavBar/Style.js
@@ -0,0 +1,23 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+     
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+      textAlign:"center"
+    },
+    appbar: {
+      backgroundColor: "#353353" 
+    }
+    
+  }));
+
+  export default useStyles
+
+  
