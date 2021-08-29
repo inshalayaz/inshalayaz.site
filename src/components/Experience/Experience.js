bb2c55d01fa8ccf405caf3f2b40d7dff3d97d8ee
inshal ayaz <inshal.ayaz14@gmail.com>
2021-08-29 16:13:05 +0500

    styling fix

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import {Typography} from '@material-ui/core'
 import useStyles from './Style'
 import {Data1, Data2} from './Timeline/Data'
 import CustomizedTimeline from './Timeline/CustomizedTimeline'
@@ -12,6 +13,11 @@ export default function Experience() {
   return (
     <div className={classes.root} id='experience'>
       <Grid container spacing={3} >
+      <Grid item xs={12} sm={12}>
+            <Typography variant="h3" align='left' style={{ color:'#fff', margin:'80px 0px 50px 50px ' }} >
+                        Services
+            </Typography>
+            </Grid>
         <Grid item xs={6}>
           <Paper className={classes.paper}>
               {
--- src/components/Experience/Style.js
+++ src/components/Experience/Style.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
      root: {
     flexGrow: 1,
+    marginTop: '150px'
   },
   paper: {
     padding: theme.spacing(2),
