d4617700443181263fb971f0694ef1b2a0a74198
inshal ayaz <inshal.ayaz14@gmail.com>
2021-09-10 21:11:54 +0500

    unused import removed

--- src/components/Experience/Timeline/CustomizedTimeline.js
+++ src/components/Experience/Timeline/CustomizedTimeline.js
@@ -4,7 +4,6 @@ import TimelineItem from '@material-ui/lab/TimelineItem';
 import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
 import TimelineConnector from '@material-ui/lab/TimelineConnector';
 import TimelineContent from '@material-ui/lab/TimelineContent';
-import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
 import TimelineDot from '@material-ui/lab/TimelineDot';
 import SchoolIcon from '@material-ui/icons/School';// import LaptopMacIcon from '@material-ui/icons/LaptopMac';
 import WorkIcon from '@material-ui/icons/Work';import Paper from '@material-ui/core/Paper';
